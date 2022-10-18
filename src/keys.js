83ca2e02e7cc1f181546de46c74ddb3ff3184121
Pratt Paredes <112298920+prattparedes@users.noreply.github.com>
2022-10-18 17:24:13 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 // WHERE TO GET THE KEY https://developers.google.com/custom-search/v1/using_rest
 // https://cse.google.com/cse/create/new
 
-export const API_KEY = "AIzaSyDLn4P_ULoMBy_HkJeJ816o7ZgMTaQrscw";
+export const API_KEY = "CONFIDENTIAL";
 
 export default API_KEY;
