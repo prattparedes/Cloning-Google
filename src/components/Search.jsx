e3202d4af2887c431220ae855345533c75b4a3d1
Pratt <pratt.paredes@gmail.com>
2022-10-17 23:10:04 -0500

    context API implemented

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,8 +4,12 @@ import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useStateValue } from "../StateProvider"
+import { actionTypes } from "../reducer"
 
 function Search({ hideButtons = false }) {
+  const [{}, dispatch] = useStateValue();
+
   const [input, setInput] = useState("");
   const navigate = useNavigate();
 
@@ -13,6 +17,11 @@ function Search({ hideButtons = false }) {
     e.preventDefault();
 
     console.log(`You searched for ${input}`);
+
+    dispatch({
+        type: actionTypes.SET_SEARCH_TERM,
+        term: input
+    })
 
     navigate("/search");
   };
